c4fb034951d377be7dc4b6cbfa4938659ec90094
Ishita Jain <35698855+ishita0127@users.noreply.github.com>
2019-10-21 18:00:29 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+/* var string = "Enter your age"; //variable names need to be clear and precise.
+var age = prompt(string);
+
+var string2 = "What is your name";
+var name = prompt(string2);
+document.write("Hello " + name + " Your age is " + age);
+
+var text = "Hello url";
+var url = "https://alison.com/courses/diploma-in-web-design/content?event=login";
+document.write(text.link(url));
+
+*/
+
+/*
+alert("Welcome to my Javascript basics practice");
+
+var string2 = "What is your name";
+var name = prompt(string2);
+
+var string = "Enter your favorite color";
+var color = prompt(string2);
+
+document.write("Hello " + name + " Your Favorite color is " + color);
+
+console.log("Program Complete");
+
+var age = prompt("enter age");
+
+var yearsAlive = parseInt(age);
+
+yearsAlive = yearsAlive * 365;
+
+document.write("Alive for " + yearsAlive + " Days!!!");
+
+var numbers  = Math.random();
+
+alert(numbers);
+
+//floor and cieling
+
+
+var numbers  = Math.floor(Math.random() * 50) + 1;
+
+alert(numbers);
+
+
+var num = prompt("enter number");
+
+num = parseInt(num);
+
+var rand = Math.floor(Math.random() * num) + 1;
+
+alert("random numbers" + rand);
+*/
